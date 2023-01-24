1838e7f58c648068dd97e3136fe8d9a99850cb42
Tomás Villarreal <toncho196kaifer@gmail.com>
2023-01-24 12:13:10 -0300

    Version 3.0

--- script.js
+++ script.js
@@ -29,13 +29,17 @@ function printScreen(number) {
 const input = document.querySelector('input');
 const log = document.getElementById('log');
 document.addEventListener('keydown', logKey);
-const arrayNumber = ["1","Digit2","Digit3","Digit4","Digit5","Digit6","Digit7","Digit8","Digit9", "Digit1"];
+const arrayNumber = ["1","2","3","4","5","Digit6","Digit7","Digit8","Digit9", "Digit1"];
 
 function logKey(e) {
     let val = e.code;
     val = val.toString();
     val = val.slice(-1);
-    printScreen(val);
+    for (let valor in arrayNumber) {
+       if(val == valor ){
+        printScreen(val);
+       }
+    }
     }
 
 buttonEqual.addEventListener("click", ()=>{
